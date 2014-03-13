2e6a2da8e7016a813cd4ed3acbb7d809e48e532b
Anthony Munene <munene.anthony@gmail.com>
2014-03-13 15:46:07 +0000

    HMTB-3306 modify script so that we can run build and test commands

--- server.sh
+++ server.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 cd assets/
 if [[ -n $1 ]]; then
-	if [ $1 == "node" ]
-	then
-		echo "Starting default grunt task..."
-		grunt
-	else
+	case "$1" in
 
-		echo "invalid parameter '$1' expecting ./start-server node"
-	fi
+	"dev") echo "Starting grunt dev mode..."
+		  grunt
+		  ;;
+	"build") echo "Starting grunt build task..."
+				grunt build
+				;;
+	"test")  echo "Starting grunt test task..."
+				grunt test
+				;;
+	*)  echo "invalid parameter '$1'"
+		;;
+	esac
 else
 	port=${1-3002}
 	echo "Starting  simple server on port $port."
