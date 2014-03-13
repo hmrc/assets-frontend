5e5948f7d27ee966ebe13021c0fbbeaa33e326cc
Anthony Munene <munene.anthony@gmail.com>
2014-03-13 14:57:03 +0000

    HMTB-3306 fail if incorrect parameter to run Grunt task runner is provided

--- start-server.sh
+++ start-server.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 cd assets/
-if [[ -n $1 && $1 -eq "node" ]]; then
-
+if [[ -n $1 ]]; then
+	if [ $1 == "node" ]
+	then
 		echo "Starting default grunt task..."
 		grunt
+	else
 
+		echo "invalid parameter '$1' expecting ./start-server node"
+	fi
 else
 	port=${1-3002}
 	echo "Starting  simple server on port $port."
