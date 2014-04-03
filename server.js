088f740c378cee23f215d53a599acdcaea130d9b
Anthony Munene <munene.anthony@gmail.com>
2014-04-03 10:38:12 +0100

    HMTB-3306 add healthcheck route for the application manager to ping

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var express = require('express');
+var fs = require ("fs");
 var app = express();
 
 app.use(express.static(__dirname + '/assets'));
 app.use(express.logger('dev'));
+app.use(app.router);
+
+//use this for the application manager healthcheck
+app.get('/public/healthcheck.json', function (req, res) {
+
+  res.send(res.statusCode)
+});
 module.exports = app;
