6dd8edd11353b433fcf85a7a5a10383e1dc30e70
Richard Vinall <richard.vinall@digital.cabinet-office.gov.uk>
2014-04-08 11:22:34 +0100

    HMTB-3671: removing rogue console.log; changing field to be hidden to be specified by classname instead of data attribute

--- assets/javascripts/modules/toggleDynamicFormFields.js
+++ assets/javascripts/modules/toggleDynamicFormFields.js
@@ -3,21 +3,20 @@ GOVUK.toggleDynamicFormFields = function () {
 
   $dynamicForm.find("*[data-dynamic-fields-hide]").each( function() {
     if($(this).prop("checked")) {
-      var $toggledField = $('*[data-' + $(this).data('dynamic-fields') + ']');
+      var $toggledField = $(".data-" + $(this).data('dynamic-fields'));
       $toggledField.addClass('js-hidden');
     }
   });
   
   $dynamicForm.on('click', '*[data-dynamic-fields]', function () {
-    var $toggledField = $('*[data-' + $(this).data('dynamic-fields') + ']')
+    var $toggledField = $(".data-" + $(this).data('dynamic-fields'));
     if ($(this).data('dynamic-fields-hide')) {
       $toggledField.addClass('js-hidden');
       $toggledField.find(':text').val('');
       $toggledField.find(':checked').prop('checked', false);
-      $toggledField.find('*[data-default]').prop('checked', true);
     } else {
       $toggledField.removeClass('js-hidden');
       $toggledField.find(':checked').prop('checked', false);
     }
   });
-}
+};
