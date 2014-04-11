979370878746a5a8e8633574bd4cce3c0fc8393a
Richard Vinall <richard.vinall@digital.cabinet-office.gov.uk>
2014-04-11 11:56:48 +0100

    HMTB-3367: cleaned up js for surveys and dynamic form fields

--- assets/javascripts/modules/toggleDynamicFormFields.js
+++ assets/javascripts/modules/toggleDynamicFormFields.js
@@ -7,14 +7,21 @@ GOVUK.toggleDynamicFormFields = function () {
       $toggledField.addClass('js-hidden');
     }
   });
-  
+
   $dynamicForm.on('click', '*[data-dynamic-fields]', function () {
     var $toggledField = $(".data-" + $(this).data('dynamic-fields'));
     if ($(this).data('dynamic-fields-hide')) {
+      if( $(this).attr('type') ==  'checkbox' ) {
+         $(this).data('dynamic-fields-hide', false);
+      }
       $toggledField.addClass('js-hidden');
       $toggledField.find(':text').val('');
       $toggledField.find(':checked').prop('checked', false);
+      $toggledField.find('*[data-default]').prop('checked', true);
     } else {
+      if( $(this).attr('type') ==  'checkbox' ) {
+         $(this).data('dynamic-fields-hide', true);
+      }
       $toggledField.removeClass('js-hidden');
       $toggledField.find(':checked').prop('checked', false);
     }
