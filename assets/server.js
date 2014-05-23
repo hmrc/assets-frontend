7fe0093c91ca161a862bafee763c35a9dbdb730d
Anthony Munene <munene.anthony@gmail.com>
2014-05-23 16:15:00 +0100

    Non-story: add route for serving assets in the public folder during development

--- assets/server.js
+++ assets/server.js
@@ -2,8 +2,9 @@ var express = require('express');
 var fs = require ("fs");
 var app = express();
 
-app.use(express.static(__dirname));
+
 app.use(express.logger('dev'));
+app.use('/assets', express.static(__dirname + '/public'))
 app.use(app.router);
 
 //use this for the application manager healthcheck
@@ -11,4 +12,6 @@ app.get('/', function (req, res) {
 
   res.send(res.statusCode)
 });
+
+
 module.exports = app;
