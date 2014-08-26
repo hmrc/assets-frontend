e2deec3c5204b8ca6eb569ac05d859d6d5e033a1
Rory Powis <rorypowis@digital.cabinet-office.gov.uk>
2014-08-26 15:41:58 +0100

    Add express paths for sourcemaps in server.js

--- assets/server.js
+++ assets/server.js
@@ -1,17 +1,19 @@
 var express = require('express');
-var fs = require ("fs");
+var fs = require("fs");
 var app = express();
 
-
 app.use(express.logger('dev'));
-app.use('/assets', express.static(__dirname + '/public'))
+
+// Path for SASS source maps
+app.use('/assets/999-SNAPSHOT/stylesheets', express.static(__dirname));
+
+// Path for static assets
+app.use('/assets', express.static(__dirname + '/public'));
 app.use(app.router);
 
 //use this for the application manager healthcheck
-app.get('/', function (req, res) {
-
+app.get('/', function(req, res) {
   res.send(res.statusCode)
 });
-
 
 module.exports = app;
