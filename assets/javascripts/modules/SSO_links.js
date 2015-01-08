cd847f2469f03f35fc5a91139d839f2320634b24
Rory Powis <rory.powis@digital.hmrc.gov.uk>
2015-01-08 14:14:27 +0000

    Replace SSO portal form with get request
    Bump version to v1.43.0

--- assets/javascripts/modules/SSO_links.js
+++ assets/javascripts/modules/SSO_links.js
@@ -1,21 +1,22 @@
 define(['jquery'], function($) {
   return function(element, ssoUrl) {
-    var a, $target,
+    var $target,
         clientSso,
         serverSso,
         successful,
-        destination,
-        payload;
+        destination;
 
     /**
      * Attach a one-time event handler for all global links
      */
     if (element) {
       $target = $(element.target);
-      
-      clientSso = $(element.target).data('sso') === true || $(element.target).data('sso') === "client";
+
+      clientSso =
+        $(element.target).data('sso') === true ||
+        $(element.target).data('sso') === "client";
       serverSso = $(element.target).data('sso') === "server";
-      a = document.createElement('a');
+
       if (clientSso || serverSso) {
         successful = true;
         destination = serverSso ? {
@@ -30,17 +31,7 @@ define(['jquery'], function($) {
           async: false,
           cache: false,
           success: function(data, status, jqXHR) {
-            var form = document.createElement('form');
-            form.method = 'GET';
-            form.action = ssoUrl;
-            payload = document.createElement('input');
-            payload.type = 'hidden';
-            payload.name = 'payload';
-            payload.value = data;
-            document.body.appendChild(form);
-            form.appendChild(payload);
-            // POST form
-            form.submit();
+            window.location = ssoUrl + '?payload=' + encodeURIComponent(data);
           },
           error: function() {
             successful = false;
