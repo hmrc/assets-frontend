aadd919bf6edebceed5a33240aed06fca6e295d8
Richard Vinall <richard.vinall@digital.hmrc.gov.uk>
2015-01-14 11:25:32 +0000

    [action-list] Making changes to fix ie7 bugs and style cleanup

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -145,5 +145,14 @@ require([
     registerBlockInputFields();
     exitSurveyValidation();
     saEmailPrefs().setup();
+
+
+    $('details').on('click', function(e){
+      console.log($(e.target));//.find('div').is(':visible'));
+      // if($(e).find('.panel-indent').showing()){
+      //   $('.action-list').find('a').css({'visibility': 'hidden'});
+      //   $('.action-list').find('a').css({'visibility': 'visible'});
+      // }
+    });
   });
 });
