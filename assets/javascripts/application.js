67ce1f6ea303a954a1040cd8771a0cca834c8291
Rory Powis <rory.powis@digital.hmrc.gov.uk>
2015-02-05 17:11:25 +0000

    add pollyfill for details & summary element functionality

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -14,133 +14,123 @@ var setSSOLinks = require('./modules/SSO_links.js'),
     registerBlockInputFields = require('./modules/registerBlockInputFields.js'),
     exitSurveyValidation = require('./modules/exitSurveyValidation.js'),
     saEmailPrefs = require('./modules/saEmailPrefs.js'),
-    GOVUK = require('stageprompt');
+    GOVUK = require('stageprompt'),
+    detailsPolyfill = require('details');
 
-(function() {
-
-  $(function() {
-    $(document).on('click', 'a', function(e) {
-      // TODO: fix error thrown when clicking a [data-sso] links
-      return setSSOLinks(e, window.ssoUrl);
-    });
-
-    var $clickableRow = $('.clickable-row'),
-        //feedback forms require a hidden field denoting if javascript is enabled
-        $feedbackForms = $('.form--feedback'),
-        $searchFocus = $('.js-search-focus'),
-        $errorReportForm = $('.report-error__content form');
-
-    if($clickableRow.length) {
-      tableRowClick($clickableRow);
-    }
-    preventDoubleSubmit();
-
-    // initialise stageprompt for Analytics
-    GOVUK.performance.stageprompt.setupForGoogleAnalytics();
-
-    // toggle for reporting a problem (on all content pages)
-    $('.report-error__toggle').on('click', function(e) {
-      $('.report-error__content').toggleClass('js-hidden');
-      e.preventDefault();
-    });
-
-
-
-    //we have javascript enabled so change hidden input to reflect this
-    $feedbackForms.find('input[name="isJavascript"]').attr("value", true);
-
-    //Initialise validation for the feedback form
-    $errorReportForm.validate({
-      errorClass: 'error-notification',
-      errorPlacement: function(error, element) {
-        error.insertBefore(element);
-      },
-      //Highlight invalid input
-      highlight: function(element, errorClass) {
-        $(element).parent().addClass('form-field--error');
-        //TODO: temp fix for form submission bug. Report a problem needs a rewrite
-        $errorReportForm.find('.button').prop('disabled', false);
-      },
-      //Unhighlight valid input
-      unhighlight: function(element, errorClass) {
-        $(element).parent().removeClass('form-field--error');
-      },
-      //When all fields are valid perform AJAX call
-      submitHandler: function(form) {
-        reportAProblem().submitForm(form, $errorReportForm.attr("action"));
-      }
-    });
-
-
-    $('.print-link a').attr('target', '_blank');
-
-    if ($searchFocus.val() !== '') {
-      $searchFocus.addClass('focus');
-    }
-    $searchFocus.on('focus', function(e) {
-      $(e.target).addClass('focus');
-    });
-    $searchFocus.on('blur', function(e) {
-      if ($searchFocus.val() === '') {
-        $(e.target).removeClass('focus');
-      }
-    });
-    if (window.location.hash && $(".design-principles").length !== 1 && $('.section-page').length !== 1) {
-      contentNudge(window.location.hash);
-    }
-    $("nav").delegate('a', 'click', function() {
-      var hash,
-          href = $(this).attr('href');
-      if (href.charAt(0) === '#') {
-        hash = href;
-      } else if (href.indexOf("#") > 0) {
-        hash = "#" + href.split("#")[1];
-      }
-      if ($(hash).length === 1) {
-        $("html, body").animate({
-          scrollTop: $(hash).offset().top - $("#global-header").height()
-        }, 10);
-      }
-    });
-
-    // hover, active and focus states for buttons in IE<8
-    if (!$.support.leadingWhitespace) {
-      $('.button').not('a')
-        .on('click focus', function() {
-          $(this).addClass('button-active');
-        })
-        .on('blur', function() {
-          $(this).removeClass('button-active');
-        });
-      $('.button')
-        .on('mouseover', function() {
-          $(this).addClass('button-hover');
-        })
-        .on('mouseout', function() {
-          $(this).removeClass('button-hover');
-        });
-    }
-
-    if ($("*[data-contextual-helper]").length) {
-      // setup showing/hiding of contextual fields
-      toggleContextualFields().setup();
-    }
-
-    toggleDynamicFormFields();
-    //TODO: replace toggleDynamicFormField usage in all exemplars and rename this function
-    simpleToggleDynamicFormFields();
-    questionnaireSubmission();
-    registerBlockInputFields();
-    exitSurveyValidation();
-    saEmailPrefs().setup();
-
-
-    $('details').on('click', function(e){
-      console.log($(e.target));//.find('div').is(':visible'));
-      // if($(e).find('.panel-indent').showing()){
-      //   $('.action-list').find('a').css({'visibility': 'hidden'});
-      //   $('.action-list').find('a').css({'visibility': 'visible'});
-      // }
-    });
+$(function() {
+  $(document).on('click', 'a', function(e) {
+    // TODO: fix error thrown when clicking a [data-sso] links
+    return setSSOLinks(e, window.ssoUrl);
   });
-})();
+
+  var $clickableRow = $('.clickable-row'),
+      //feedback forms require a hidden field denoting if javascript is enabled
+      $feedbackForms = $('.form--feedback'),
+      $searchFocus = $('.js-search-focus'),
+      $errorReportForm = $('.report-error__content form');
+
+  if($clickableRow.length) {
+    tableRowClick($clickableRow);
+  }
+  preventDoubleSubmit();
+
+  // initialise stageprompt for Analytics
+  GOVUK.performance.stageprompt.setupForGoogleAnalytics();
+
+  // toggle for reporting a problem (on all content pages)
+  $('.report-error__toggle').on('click', function(e) {
+    $('.report-error__content').toggleClass('js-hidden');
+    e.preventDefault();
+  });
+
+
+
+  //we have javascript enabled so change hidden input to reflect this
+  $feedbackForms.find('input[name="isJavascript"]').attr("value", true);
+
+  //Initialise validation for the feedback form
+  $errorReportForm.validate({
+    errorClass: 'error-notification',
+    errorPlacement: function(error, element) {
+      error.insertBefore(element);
+    },
+    //Highlight invalid input
+    highlight: function(element, errorClass) {
+      $(element).parent().addClass('form-field--error');
+      //TODO: temp fix for form submission bug. Report a problem needs a rewrite
+      $errorReportForm.find('.button').prop('disabled', false);
+    },
+    //Unhighlight valid input
+    unhighlight: function(element, errorClass) {
+      $(element).parent().removeClass('form-field--error');
+    },
+    //When all fields are valid perform AJAX call
+    submitHandler: function(form) {
+      reportAProblem().submitForm(form, $errorReportForm.attr("action"));
+    }
+  });
+
+
+  $('.print-link a').attr('target', '_blank');
+
+  if ($searchFocus.val() !== '') {
+    $searchFocus.addClass('focus');
+  }
+  $searchFocus.on('focus', function(e) {
+    $(e.target).addClass('focus');
+  });
+  $searchFocus.on('blur', function(e) {
+    if ($searchFocus.val() === '') {
+      $(e.target).removeClass('focus');
+    }
+  });
+  if (window.location.hash && $(".design-principles").length !== 1 && $('.section-page').length !== 1) {
+    contentNudge(window.location.hash);
+  }
+  $("nav").delegate('a', 'click', function() {
+    var hash,
+        href = $(this).attr('href');
+    if (href.charAt(0) === '#') {
+      hash = href;
+    } else if (href.indexOf("#") > 0) {
+      hash = "#" + href.split("#")[1];
+    }
+    if ($(hash).length === 1) {
+      $("html, body").animate({
+        scrollTop: $(hash).offset().top - $("#global-header").height()
+      }, 10);
+    }
+  });
+
+  // hover, active and focus states for buttons in IE<8
+  if (!$.support.leadingWhitespace) {
+    $('.button').not('a')
+      .on('click focus', function() {
+        $(this).addClass('button-active');
+      })
+      .on('blur', function() {
+        $(this).removeClass('button-active');
+      });
+    $('.button')
+      .on('mouseover', function() {
+        $(this).addClass('button-hover');
+      })
+      .on('mouseout', function() {
+        $(this).removeClass('button-hover');
+      });
+  }
+
+  if ($("*[data-contextual-helper]").length) {
+    // setup showing/hiding of contextual fields
+    toggleContextualFields().setup();
+  }
+
+  toggleDynamicFormFields();
+  //TODO: replace toggleDynamicFormField usage in all exemplars and rename this function
+  simpleToggleDynamicFormFields();
+  questionnaireSubmission();
+  registerBlockInputFields();
+  exitSurveyValidation();
+  saEmailPrefs().setup();
+  detailsPolyfill;
+});
