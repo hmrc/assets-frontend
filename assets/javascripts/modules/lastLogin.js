5be4a5a13112c327a22860e5552ed03ba7943db3
Sean McGowan <smcgowan@gmail.com>
2015-02-17 10:11:18 +0000

    [YTA-628] Last login section of YTA dashboard, initial files and set up.

---
+++ assets/javascripts/modules/lastLogin.js
@@ -0,0 +1,16 @@
+/**
+ * YTA Dashboard - Last Login Section
+ *
+ * This JS implementation is currently necessary as the details/summary tags do not support
+ * what the design required, which was to place text before a right-aligned summary tag (YTA-628)
+ *
+ */
+
+(function() {
+
+
+
+
+
+
+})();
