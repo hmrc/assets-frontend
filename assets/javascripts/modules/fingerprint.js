6475472a1fdc59244b977de9f01a9f2b40b58c3f
Ben Avery <spacebaboon@gmail.com>
2015-03-17 14:49:51 +0000

    YTA-829: replace browser's native btoa encoding with js-base64 npm module for old IE compatibility.
    Author:    Ben Avery <ben.avery@digital.hmrc.gov.uk>

--- assets/javascripts/modules/fingerprint.js
+++ assets/javascripts/modules/fingerprint.js
@@ -1,13 +1,15 @@
 var Mdtpdf = require('mdtpdf'),
-    govuk = require('./GOVUK_helpers.js');
+  govuk = require('./GOVUK_helpers.js'),
+  base64 = require('base64').Base64;
 
-module.exports =  function() {
+module.exports = function () {
   // TODO: obscure all references to fingerprint
   var fingerprint = new Mdtpdf({
-        screen_resolution: true
+      screen_resolution: true,
+      ie_activex: true
     }),
-    encodedFingerPrint = window.btoa(fingerprint.get()),
-    mdtpdfCookie = govuk.getCookie("mdtpdf");
+    mdtpdfCookie = govuk.getCookie("mdtpdf"),
+    encodedFingerPrint = base64.btoa(fingerprint.get());
 
   // IE7: Do not set the cookie when the encoded fingerprint is empty in IE7
   // to prevent the session being cleared by the server
