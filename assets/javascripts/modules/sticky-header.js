be34e4fa5b81e6b9deba21ea0c01c01e2e5449a5
Rory Powis <rory.powis@digital.hmrc.gov.uk>
2015-04-09 10:58:23 +0100

    [AOSS-231] Convert sticky-header to custom module
    - Rely on inherited left value
    - Use class instead of setting css with js.
    - jQuerify for cross-browser ease
    - Fix client-list table widths

---
+++ assets/javascripts/modules/sticky-header.js
@@ -0,0 +1,47 @@
+require('jquery');
+
+function getWindowScroll() {
+  return {
+    top: window.pageYOffset || document.documentElement.scrollTop,
+    left: window.pageXOffset || document.documentElement.scrollLeft
+  };
+}
+
+function calcRect(el) {
+  var rect = el.getBoundingClientRect(),
+      windowScroll = getWindowScroll(),
+      style = el.currentStyle || window.getComputedStyle(el);
+
+  return {
+    top: rect.top + windowScroll.top,
+    width: rect.width,
+    height: rect.height + parseInt(style.marginTop) + parseInt(style.marginBottom)
+  };
+}
+
+module.exports = function(settings) {
+  'use strict';
+
+  var el = $(settings.el)[0],
+      className = settings.className,
+      requiredTop = settings.top || 0,
+      originalRect = calcRect(el),
+      placeholder = document.createElement('div');
+
+  placeholder.id = 'placeholder';
+  placeholder.style.width = originalRect.width + 'px';
+  placeholder.style.height = originalRect.height + 'px';
+
+  $(window).scroll(function(event) {
+    if (getWindowScroll().top > originalRect.top - requiredTop) {
+      $(el).addClass(className).after(function() {
+        if (!document.getElementById('placeholder')) {
+          return placeholder;
+        }
+      });
+    } else {
+      $(el).removeClass(className);
+      $(placeholder).remove();
+    }
+  });
+};
