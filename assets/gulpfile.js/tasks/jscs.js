83b9ec00380d3400c9ceb4336e996e0adabeeb5b
Anthony Munene <munene.anthony@gmail.com>
2015-04-27 11:35:08 +0100

    FE-18 concat and jscs tasks

---
+++ assets/gulpfile.js/tasks/jscs.js
@@ -0,0 +1,14 @@
+'use strict';
+var gulp =  require('gulp'),
+	config = require('../config').scripts,
+	jscs = require('gulp-jscs');
+
+gulp.task('jscs', function() {
+	return gulp.src([
+		config.entryPoint,
+		config.src,
+		config.gulpTasks,
+		config.jshintExclude
+	])
+	.pipe(jscs(config.jscs));
+});
