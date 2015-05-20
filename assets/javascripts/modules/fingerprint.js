fb8e593c0dfc6abee8ff0a688c35ef44e8204cbd
Rory Powis <rory.powis@digital.hmrc.gov.uk>
2015-05-20 17:58:51 +0100

    Set more general domain on mdtpdf cookie

--- assets/javascripts/modules/fingerprint.js
+++ assets/javascripts/modules/fingerprint.js
@@ -4,16 +4,21 @@ var Mdtpdf = require('mdtpdf'),
 
 module.exports = function() {
   // TODO: obscure all references to fingerprint
-  var fingerprint = new Mdtpdf({
-      screen_resolution: true,
-      ie_activex: true
-    }),
-    mdtpdfCookie = govuk.getCookie('mdtpdf'),
-    encodedFingerPrint = base64.btoa(fingerprint.get());
+  var curDomain,
+      cookieDomain,
+      fingerprint = new Mdtpdf({
+        screen_resolution: true,
+        ie_activex: true
+      }),
+      mdtpdfCookie = govuk.getCookie('mdtpdf'),
+      encodedFingerPrint = base64.btoa(fingerprint.get());
 
   // IE7: Do not set the cookie when the encoded fingerprint is empty in IE7
   // to prevent the session being cleared by the server
   if (!mdtpdfCookie && encodedFingerPrint) {
-    govuk.setCookie('mdtpdf', encodedFingerPrint, 7300);
+    curDomain = window.location.hostname.split('.').slice(-3);
+    cookieDomain = (curDomain.length > 1) ? '.' + curDomain.join('.') : window.location.hostname;
+
+    govuk.setCookie('mdtpdf', encodedFingerPrint, 7300, cookieDomain);
   }
 };
