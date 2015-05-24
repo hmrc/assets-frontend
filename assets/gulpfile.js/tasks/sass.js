64970aae6914c65ae93d7d4349a1d098f8b5735e
Anthony Munene <munene.anthony@gmail.com>
2015-05-24 12:05:25 +0100

    FE-18 add auto-prefixer

--- assets/gulpfile.js/tasks/sass.js
+++ assets/gulpfile.js/tasks/sass.js
@@ -1,10 +1,12 @@
 'use strict';
 
-var gulp        = require('gulp'),
-    gulpif      = require('gulp-if'),
-    sass        = require('gulp-sass'),
-    config      = require('../config').sass,
-    sourceMaps  = require('gulp-sourcemaps');
+var gulp         = require('gulp'),
+    gulpif       = require('gulp-if'),
+    sass         = require('gulp-sass'),
+    config       = require('../config').sass,
+    sourceMaps   = require('gulp-sourcemaps'),
+    autoprefixer = require('gulp-autoprefixer'),
+    rename      = require('gulp-rename');
 
 gulp.task('sass', function() {
   var env   = global.runmode,
@@ -13,6 +15,8 @@ gulp.task('sass', function() {
   return gulp.src(config.src)
        .pipe(gulpif(isDev, sourceMaps.init()))
        .pipe(sass(config[env].settings))
+       .pipe(autoprefixer())
+       .pipe(rename({suffix: '.min'}))
        .pipe(gulpif(isDev, sourceMaps.write(config[env].sourceMapsDir)))
        .pipe(gulp.dest(config[env].dest));
 });
