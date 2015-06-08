109942c317871784023a271173d24f7660e8f971
Rory Powis <rory.powis@digital.hmrc.gov.uk>
2015-06-08 18:15:57 +0100

    Add modernizr to build task and make environment conditional

--- assets/gulpfile.js/tasks/modernizr.js
+++ assets/gulpfile.js/tasks/modernizr.js
@@ -7,5 +7,13 @@ var gulp         = require('gulp'),
 
 gulp.task('modernizr', function() {
   var env = global.runmode;
+
+  config.scripts.modernizr.dest = config.scripts.vendorDest[env] + config.scripts.modernizr.dest;
+
+  if(env === 'prod') {
+    config.scripts.modernizr.cache = false;
+    config.scripts.modernizr.uglify = true;
+  }
+
   return modernizr(config.scripts.modernizr);
 });
