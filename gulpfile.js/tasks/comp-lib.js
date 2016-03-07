71a8c29a744b2eac78b85bb6d13b2dc7f763bac7
Ben Chidgey <ben.chidgey@digital.hmrc.gov.uk>
2016-03-07 15:44:43 +0000

    component-library: add JavaScript location, add browserify in pre-tasks

--- gulpfile.js/tasks/comp-lib.js
+++ gulpfile.js/tasks/comp-lib.js
@@ -10,12 +10,12 @@ gulp.task('clean-comp-lib', function(cb) {
   del(compLibConfig.destination, cb);
 });
 
-gulp.task('component-library', ['clean-comp-lib', 'sass', 'images'], function (cb) {
+gulp.task('component-library', ['clean-comp-lib', 'sass', 'images', 'browserify'], function (cb) {
   var env = global.runmode,
       genCompLib = './node_modules/.bin/kss-node --config component-lib.json';
 
   exec(genCompLib, function (err, stout, sterr) {
-    gulp.src([config.images[env].dest + '/**/*', config.sass[env].dest + '**/*'], {base: config[env].dest})
+    gulp.src([config.images[env].dest + '/**/*', config.sass[env].dest + '**/*', config.scripts[env].dest + '/**/*'], {base: config[env].dest})
       .pipe(gulp.dest(compLibConfig.destination + '/public'));
     cb(err);
   });
