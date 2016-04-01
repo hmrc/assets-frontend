90a7fbd803783fa5e1afa846b009192ad2380bd0
Ben Chidgey <ben.chidgey@digital.hmrc.gov.uk>
2016-04-01 13:03:20 +0100

    add-remove-refactor: bring template script functionality into JavaScript, add ability to send in custom delete and add button text, added ablity to turn on/off delete functionaility, updated usage comments, fix bug to do with order of inputs and deletion

--- assets/javascripts/modules/addRemove.js
+++ assets/javascripts/modules/addRemove.js
@@ -1,233 +1,166 @@
-/**
- * Add/Remove Module
- *
- * Usage:
- *
- *  <div class="add-remove"
- *        data-add-remove
- *        data-add-remove-max="5"
- *        data-attr-name="redirectUris[$]">
- *
- *    <script type="text/template" data-item-template>
- *      <li class="add-remove__item" data-add-remove-item>
- *        <input type="text" value="" data-add-remove-unique/>
- *      </li>
- *    </script>
- *    <script type="text/template" data-remove-btn-template>
- *      <a href="#" class="add-remove__remove-btn" data-remove-btn>Delete</a>
- *    </script>
- *    <script type="text/template" data-add-btn-template>
- *      <a href="#" data-add-btn>Add another redirect URI</a>
- *    </script>
- *
- *    <ul data-add-remove-list>
- *      <li data-add-remove-item>
- *        <input type="text" value="Value 1" name="redirectUris[0]" data-add-remove-unique/>
- *      </li>
- *      <li data-add-remove-item>
- *        <input type="text" value="Value 2" name="redirectUris[1]" data-add-remove-unique/>
- *      </li>
- *      <li data-add-remove-item>
- *        <input type="text" value="Value 3" name="redirectUris[2]" data-add-remove-unique/>
- *      </li>
- *    </ul>
- *
- *  </div>
- *
+/*
+Add Remove
+
+A component to add and remove inputs on a page. You have the option of sending in custom attributes for:
+- `data-can-delete="true"` to add delete buttons
+- `data-delete-btn-text` to send in custom delete button text, this defaults to **Delete** and will only work when the
+ `data-can-delete="true"` is set
+- `data-add-btn-text` to send in custom add button text, this defaults to **Add another redirect URI**
+- `data-max` Maximum items allowed in the list
+
+Markup:
+<div data-add-remove
+     data-max="5"
+     data-can-delete="true",
+     data-delete-btn-text="Delete Me"
+     data-add-btn-text="Add Me">
+  <ul class="add-remove__list" data-add-remove-list>
+    <li class="add-remove__item" data-add-remove-item>
+      <input name="example[]"
+             type="text"
+             class="form-input input--medium"
+             data-add-remove-input />
+    </li>
+    <li class="add-remove__item" data-add-remove-item>
+      <input name="example[]"
+             type="text"
+             class="form-input input--medium"
+             data-add-remove-input />
+    </li>
+  </ul>
+</div>
  */
 
+var DEFAULT_ADD_BTN_TEXT = 'Add another redirect URI',
+    DEFAULT_DELETE_BTN_TEXT = 'Delete',
+    addRemoveContainer = '[data-add-remove]',
+    addRemoveList = '[data-add-remove-list]',
+    addRemoveItem = '[data-add-remove-item]',
+    addRemoveInput = '[data-add-remove-input]',
+    addButton = '[data-add-btn]',
+    maxItems = 'data-max';
 
-var addRemoveContainer = '[data-add-remove]',
-    addRemoveList      = '[data-add-remove-list]',
-    addRemoveItem      = '[data-add-remove-item]',
-    addButton          = '[data-add-btn]',
-    removeButton       = '[data-remove-btn]',
-    maxItems           = 'data-add-remove-max';
-
-var itemTemplate       = '[data-item-template]',
-    addBtnTemplate     = '[data-add-btn-template]',
-    removeBtnTemplate  = '[data-remove-btn-template]';
-
-
-/**
- * Init add/remove, where necessary
- *
- * @param
- */
-module.exports = function() {
-
+var init = function () {
   var $addRemoveContainers = $(addRemoveContainer);
 
-  // only continue if counter html exists
-  if($addRemoveContainers.length === 0) return;
+  if ($addRemoveContainers.length) {
 
-  // for each add/remove container on the page
-  $addRemoveContainers.each(function() {
-    var $container = $(this);
+    $addRemoveContainers.each(function (i, container) {
+      var $container = $(container);
 
-    $container.find('[data-removable]').each(function() {
-      var $item = $(this);
+      if (deleteIsEnabled($container)) {
+        $container.find(addRemoveItem).each(function (index, listItem) {
+          if (index > 0) {
+            addDeleteBtn($(listItem), $container);
+          }
+        });
+      }
 
-      // add html for remove button
-      insertRemoveButton($container, $item);
-
-      // bind remove click event
-      bindRemoveEvent($container, $item);
-
+      // only insert Add button if list has space
+      if (!maxLengthReached($container)) {
+        insertAddButton($container);
+      }
     });
-
-    // only insert Add button if list has space
-    if(!isListFull($container)) {
-
-      // add html for add button
-      insertAddButton($container);
-
-    }
-
-    // bind add click
-    bindAddEvent($container);
-
-  });
-
-};
-
-/**
- * Insert a Remove button in the item
- *
- * @param $container
- * @param $item
- */
-var insertRemoveButton = function($container, $item) {
-
-  var html = $container.find(removeBtnTemplate).html();
-
-  $item.append(html);
-
-};
-
-/**
- * Bind click event on Remove button for an item
- *
- * @param $container
- * @param $item
- */
-var bindRemoveEvent = function($container, $item) {
-
-  $item.on('click', removeButton, function(e) {
-    e.preventDefault();
-
-    // remove list item
-    $item.remove();
-
-    // if there's no Add button
-    if($container.find(addButton).length === 0) {
-
-      // insert Add button as there must be at least one available slot
-      insertAddButton($container);
-
-    }
-
-  });
-
+  }
 };
 
 /**
  * Insert a Add button after the list of items
- *
  * @param $container
  */
-var insertAddButton = function($container) {
+var insertAddButton = function ($container) {
+  var addBtnText = $container.attr('data-add-btn-text') || DEFAULT_ADD_BTN_TEXT;
+  var $addInputBtn = $('<a href="#" data-add-btn>' + addBtnText + '</a>')
+    .click(function (event) {
+      event.preventDefault();
+      addListItem($container);
+    });
 
-  var html = $container.find(addBtnTemplate).html();
-
-  $container.append(html);
-
+  $container.append($addInputBtn);
 };
 
 /**
- * Bind a click event to the Add button
- *
+ * Show the add input button
  * @param $container
  */
-var bindAddEvent = function($container) {
-
-  $container.on('click', addButton, function(e) {
-
-    e.preventDefault();
-
-    addItem($container);
-
-  });
-
+var showAddButton = function ($container) {
+  if (!maxLengthReached($container)) {
+    $container.find(addButton).show();
+  }
 };
 
-
 /**
- * Add a new item to the list
- *
+ * Hide the add input button
  * @param $container
  */
-var addItem = function($container) {
-
-  var $list    = $container.find(addRemoveList),
-      $newItem = createItem($container);
-
-  // only insert Remove button if item should be removable
-  if($newItem.is('[data-removable]')) {
-
-    // add the Remove button
-    insertRemoveButton($container, $newItem);
-
-    // bind click event on Remove button
-    bindRemoveEvent($container, $newItem);
-
+var hideAddButton = function ($container) {
+  if (maxLengthReached($container)) {
+    $container.find(addButton).hide();
   }
-
-  // append new item to list
-  $list.append($newItem);
-
-  // if max length reached
-  if(isListFull($container)) {
-    $container.find(addButton).remove();
-  }
-
 };
 
-
-
 /**
- * Create new item based off template
+ * Add a new list item to list
+ * @param $container
  */
-var createItem = function($container) {
+var addListItem = function ($container) {
+  var $list = $container.find(addRemoveList),
+      $lisItem = createItem($container);
 
-  var listCount = $container.find('[data-add-remove-item]').length,      // get count of items
-      nameAttr  = $container.attr('data-attr-name'),                     // get pattern for unique name attribute
-      $newItem,
-      $unique;
-
-  // get clone of template item wrapped in LI
-  $newItem = $($container.find(itemTemplate).html());
-
-  // get unique item (e.g. input, textarea) within item
-  $unique = $newItem.find('[data-add-remove-unique]');
-
-  // prepare unique attributes for unique element (e.g. input, textarea), if specified
-  if(nameAttr) {
-    nameAttr = nameAttr.replace(/\$/, listCount);
-    $unique.attr({name: nameAttr});
-  }
-
-  return $newItem;
-
+  $list.append($lisItem);
+  hideAddButton($container);
 };
 
 
 /**
- * Returns whether or not list is full based on maxItems attribute
- *
+ * Create new list item containing input. If the attribute data-can-delete="true" has been added to the container
+ * then the list item will have the delete functionality added.
+ * @param $container
+ * @returns {*}
+ */
+var createItem = function ($container) {
+  var $listItemClone,
+      $input;
+
+  $listItemClone = $container.find(addRemoveItem).first().clone();
+  $input = $listItemClone.find(addRemoveInput);
+  $input.val('');
+
+  if (deleteIsEnabled($container)) {
+    addDeleteBtn($listItemClone, $container);
+  }
+
+  return $listItemClone;
+};
+
+var deleteIsEnabled = function ($container) {
+  return !!$container.attr('data-can-delete');
+};
+
+var addDeleteBtn = function ($listItem, $container) {
+  var deleteBtnText = $container.attr('data-delete-btn-text') || DEFAULT_DELETE_BTN_TEXT,
+      $deleteBtn;
+
+  $deleteBtn = $('<a href="#" class="add-remove__remove-btn" data-remove-btn>' + deleteBtnText + '</a>')
+    .click(function (event) {
+      event.preventDefault();
+      $listItem.remove();
+      showAddButton($container);
+    });
+
+  $listItem.append($deleteBtn);
+};
+
+/**
+ * Returns whether or not list is full based on data-max attribute
  * @param $container
  * @returns {boolean}
  */
-var isListFull = function($container) {
-  return $container.find(addRemoveItem).length === parseInt($container.attr(maxItems), 10);
+var maxLengthReached = function ($container) {
+  var listLength = $container.find(addRemoveItem).length;
+  var maxListsAllowed = parseInt($container.attr(maxItems), 10);
+
+  return listLength === maxListsAllowed;
 };
+
+module.exports = init;
