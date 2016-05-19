a1b68bc98f713069b4c36631b0f31d75c9276782
Rory Powis <rory.powis@digital.hmrc.gov.uk>
2016-05-19 14:17:56 +0100

    Fail the build if stylelint errors

--- gulpfile.js/tasks/stylelint.js
+++ gulpfile.js/tasks/stylelint.js
@@ -1,16 +1,24 @@
 'use strict';
 
 var gulp = require('gulp'),
+    gulpif = require('gulp-if'),
     gutil = require('gulp-util'),
     config = require('../config').sass,
+    plumber = require('gulp-plumber'),
     stylelint = require('gulp-stylelint');
 
 gulp.task('stylelint', function() {
+  var env   = global.runmode,
+      isDev = (env === 'dev');
+
   return gulp
     .src(config.src)
-    .on('error', gutil.log)
+    .pipe(gulpif(isDev, plumber(function(error) {
+      gutil.log(gutil.colors.red(error.message));
+      this.emit('end');
+    })))
     .pipe(stylelint({
-      failAfterError: false,
+      failAfterError: !isDev,
       reporters: [{
         formatter: 'string',
         console: true
