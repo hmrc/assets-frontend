22bce2b911e0b19bca5d2f19c23c2febcaa1fccb
Ben Chidgey <ben.chidgey@digital.hmrc.gov.uk>
2016-11-23 15:38:50 +0000

    generate the backstop config to run visual regression tests against the component library

---
+++ gulpfile.js/util/backstop/configGenerator.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var fs =  require('fs');
+var config = require('./../../config');
+var BuildScenarios = require('./buildScenarios');
+
+var getCompLibPaths = function () {
+  var files = fs.readdirSync(config.compLib.baseDir);
+
+  return files.filter(function (file) {
+    return file !== 'index.html' && file.includes('.html');
+  });
+}
+
+module.exports = function () {
+  var compLibPaths = getCompLibPaths();
+  var addScenarios = new BuildScenarios({objectMode: true}, compLibPaths);
+  var readConfig = fs.createReadStream(config.vrt.backstopConfigTemplate);
+  var writeConfig = fs.createWriteStream(config.vrt.backstopConfig);
+
+  return new Promise(function (resolve, reject) {
+    readConfig.setEncoding('utf8')
+    readConfig
+      .pipe(addScenarios)
+      .pipe(writeConfig)
+      .on('finish', function () {
+        resolve('backstop.json created')
+      })
+      .on('error', function (err) {
+        reject(err)
+      });
+  });
+};
