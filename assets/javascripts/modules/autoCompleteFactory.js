443659376ce9653d42388ce55a0a5cb4fe461f5c
adamliptrot-oc <adam.liptrot@opencastsoftware.com>
2016-11-28 14:43:56 +0000

    Allow AutoComplete to use select as source of suggestions

--- assets/javascripts/modules/autoCompleteFactory.js
+++ assets/javascripts/modules/autoCompleteFactory.js
@@ -2,19 +2,28 @@ require('jquery');
 var autoComplete = require('./autoComplete.js');
 
 var createAutoCompleteCountries = function () {
+
   var $chooseCountryAutoCompleteElem = $('.js-choose-country-auto-complete');
-  var $countryCodeInput = $('#countryCode');
-  var $targets = $chooseCountryAutoCompleteElem.first().data('targets') ? $chooseCountryAutoCompleteElem.first().data('targets') : $countryCodeInput;
+    var $countryCodeInput = $('#countryCode');
+    var suggestionDisplayTemplate = function (title, value) {
+      return title + ' (+' + value + ')';
+    };
+
+    if ($chooseCountryAutoCompleteElem.length && countries) {
+      autoComplete($chooseCountryAutoCompleteElem.first(), countries, $countryCodeInput, suggestionDisplayTemplate);
+    }
+
+};
+
+var createAutoComplete = function () {
+  // additional work required to extract the template to a configurable option
   var suggestionDisplayTemplate = function (title, value) {
     return title + ' (+' + value + ')';
   };
-
-  if ($chooseCountryAutoCompleteElem.length && countries) {
-    autoComplete($chooseCountryAutoCompleteElem.first(), countries, $targets, suggestionDisplayTemplate);
-  }
+  autoComplete($('.js-hmrc-auto-complete').first(), null, null, suggestionDisplayTemplate);
 };
 
 module.exports = function () {
   createAutoCompleteCountries();
-  // add other auto completes here
+  createAutoComplete();
 };
