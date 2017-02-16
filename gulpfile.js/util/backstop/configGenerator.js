c2255ae5d0d3eb9722b100cc3f86962d6712168a
Rory Powis <rory.powis@digital.hmrc.gov.uk>
2017-02-16 19:38:28 +0100

    Remove VRT browser report on CI

--- gulpfile.js/util/backstop/configGenerator.js
+++ gulpfile.js/util/backstop/configGenerator.js
@@ -4,6 +4,26 @@ var fs = require('fs')
 var config = require('./../../config')
 var BuildScenarios = require('./buildScenarios')
 
+// var Transform = require('stream').Transform
+// var util = require('util')
+
+var RemoveBrowserReportOnCI = function (options) {
+  Transform.call(this, options)
+}
+
+util.inherits(RemoveBrowserReportOnCI, Transform)
+
+RemoveBrowserReportOnCI.prototype._transform = function (chunk, enc, cb) {
+  var json = JSON.parse(chunk)
+
+  if (process.env.COMMIT) {
+    json.report.splice(json.report.indexOf('browser'), 1)
+  }
+
+  this.push(JSON.stringify(json))
+  cb()
+}
+
 var getCompLibPaths = function () {
   var files = fs.readdirSync(config.compLib.baseDir)
 
@@ -17,10 +37,12 @@ module.exports = function () {
   var addScenarios = new BuildScenarios({objectMode: true}, compLibPaths)
   var readConfig = fs.createReadStream(config.vrt.backstopConfigTemplate)
   var writeConfig = fs.createWriteStream(config.vrt.backstopConfig)
+  // var removeBrowserReportOnCI = new RemoveBrowserReportOnCI({objectMode: true})
 
   return new Promise(function (resolve, reject) {
     readConfig.setEncoding('utf8')
     readConfig
+      .pipe(removeBrowserReportOnCI)
       .pipe(addScenarios)
       .pipe(writeConfig)
       .on('finish', function () {
