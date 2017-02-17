314813339e91b1750e6da0455cbf25b7c96708f4
Rory Powis <rory.powis@digital.hmrc.gov.uk>
2017-02-17 09:02:23 +0100

    Wrap vrts up in build for CI

--- server.sh
+++ server.sh
@@ -9,35 +9,39 @@ deps() {
   npm install
 }
 
+runVrt() {
+  # make sure we're not on master
+  BRANCH=$(git branch | grep \* | cut -d ' ' -f2)
+  if [ "$BRANCH" = "master" ]; then
+    output "Vrts not run on $BRANCH branch."
+    exit 0
+  fi
+
+  # Store some vars for later
+  COMMIT=${TRAVIS_PULL_REQUEST_BRANCH:-$BRANCH} \
+  BRANCHPOINT=$(git merge-base master HEAD) &&
+
+  # run VRTs
+  git checkout $BRANCHPOINT &&
+  npm run vrt:baseline &&
+  git checkout $COMMIT &&
+  npm run vrt:compare
+}
+
 if [[ -n $1 ]]; then
   case "$1" in
 
   "dev") deps && output "Starting gulp in dev mode..."
     npm run dev
     ;;
-  "vrt") output "Starting vrts..."
-    # make sure we're not on master
-    BRANCH=$(git branch | grep \* | cut -d ' ' -f2)
-    if [ "$BRANCH" = "master" ]; then
-      output "Vrts not run on $BRANCH branch."
-      exit 0
-    fi
-
-    # Store some vars for later
-    COMMIT=${TRAVIS_PULL_REQUEST_BRANCH:-$BRANCH} \
-    BRANCHPOINT=$(git merge-base master HEAD) &&
-
-    # run VRTs
-    git checkout $BRANCHPOINT &&
-    npm run vrt:baseline &&
-    git checkout $COMMIT &&
-    npm run vrt:compare
+  "vrt") deps && output "Starting vrts..."
+    runVrt
     ;;
   "build") deps && output "Starting gulp build task..."
     if [[ -n $2 ]]; then
-      npm run build $2
+      runVrt && npm run build $2
     else
-      npm run build
+      runVrt && npm run build
     fi
     ;;
   "test-dev") deps && output "Auto watch tests..."
