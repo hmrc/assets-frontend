7d27a0ea6515665e2072d994a11a78dfc91bb78f
Rory Powis <rory.powis@digital.hmrc.gov.uk>
2017-03-02 12:42:26 +0100

    Add changelog task and run it as part of build

---
+++ gulpfile.js/tasks/changelog.js
@@ -0,0 +1,59 @@
+'use strict'
+
+var gulp = require('gulp')
+var gutil = require('gulp-util')
+var proc = require('child_process')
+
+var runCommand = function (cmd) {
+  return new Promise(function (resolve, reject) {
+    proc.exec(cmd, function (err, stdout, stderr) {
+      if (err) {
+        reject(err)
+      } else if (stderr) {
+        reject(stderr)
+      } else {
+        resolve(stdout)
+      }
+    })
+  })
+}
+
+var getCurrentBranch = function (travisBranch) {
+  if (travisBranch) {
+    return travisBranch
+  }
+
+  var cmd = 'git symbolic-ref HEAD | sed \'s!refs/heads/!!\''
+  return runCommand(cmd)
+}
+
+var getChangedFiles = function (branch) {
+  if (!branch) {
+    return new Error(gutil.log(gutil.colors.red('ERROR: No branch given')))
+  }
+
+  var cmd = 'git diff --name-only master ' + branch
+  return runCommand(cmd)
+}
+
+var checkForChangelog = function (files) {
+  if (!files.includes('CHANGELOG.md')) {
+    throw new Error(gutil.log(gutil.colors.red('ERROR: No CHANGELOG.md update')))
+  }
+
+  return true
+}
+
+gulp.task('changelog', function (done) {
+  getCurrentBranch(process.env.TRAVIS_PULL_REQUEST_BRANCH)
+    .then(getChangedFiles)
+    .then(checkForChangelog)
+    .catch(done)
+})
+
+module.exports = {
+  runCommand: runCommand,
+  getCurrentBranch: getCurrentBranch,
+  getChangedFiles: getChangedFiles,
+  checkForChangelog: checkForChangelog
+}
