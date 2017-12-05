9d2a9ec922821cca2e635b2ba952790c32509591
csabascript <balint.csaba.imre@gmail.com>
2017-12-05 17:07:19 +0000

    move path resolution into variable for svg task

--- gulpfile.js/tasks/svg.js
+++ gulpfile.js/tasks/svg.js
@@ -7,7 +7,9 @@ const svgmin = require('gulp-svgmin')
 const config = require('../config')
 
 gulp.task('svg', () => {
+  const dest = path.join(config.dest[gutil.env.version], config.images.destDirName)
+
   return gulp.src(config.svg.src)
     .pipe(svgmin())
-    .pipe(gulp.dest(path.join(config.dest[gutil.env.version], config.images.destDirName)))
+    .pipe(gulp.dest(dest))
 })
