ea13e58412d1306f51ef9c2d646456750bdee61b
Rory Powis <rory.powis@digital.hmrc.gov.uk>
2018-01-16 16:23:36 +0100

    Fix missing v4 scss mixins

--- assets/components/index.js
+++ assets/components/index.js
@@ -1,8 +1,11 @@
+'use strict'
 
 var accountMenu = require('./account-menu/account-menu')
 var charCounter = require('./character-counter/character-counter')
-var timeoutDialog = require('../components/timeout-dialog/timeoutDialog.js')
+var timeoutDialog = require('./timeout-dialog/timeoutDialog')
 
-accountMenu()
-charCounter()
-$.timeoutDialog = timeoutDialog
+;(function ($, window, document) {
+  accountMenu()
+  charCounter()
+  $.timeoutDialog = timeoutDialog
+})(window.jQuery, window, document)
