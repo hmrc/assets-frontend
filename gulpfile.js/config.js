43c6ca74f4c151f01caadcaeb20ea8a8376f0558
Ian Leggett <ileggett@equalexperts.com>
2018-01-29 17:37:49 +0000

    Added promises to stop race conditions with browser sync

--- gulpfile.js/config.js
+++ gulpfile.js/config.js
@@ -19,7 +19,6 @@ Test:
 - Continuous
 - Spec reporter
 */
-
 const src = './assets/'
 const dest = src + 'public/'
 const distDir = src + 'dist/'
@@ -27,9 +26,6 @@ const snapshotDir = {
   v3: dest + 'v3-SNAPSHOT/',
   v4: dest + 'v4-SNAPSHOT/'
 }
-
-const componentLibraryPort = 9033
-const patternLibraryPort = 9034
 
 var govuk = {
   elements: 'node_modules/govuk-elements-sass',
@@ -202,25 +198,6 @@ module.exports = {
           '/assets': dest
         }
       }
-    },
-    v3: {
-      ui: false,
-      port: 9033,
-      open: false,
-      logLevel: 'silent',
-      server: 'component-library'
-    },
-    v4: {
-      ui: false,
-      port: 9034,
-      open: false,
-      logLevel: 'silent',
-      server: 'design-pattern-library'
     }
-  },
-  serverMessage: {
-    v4: `Design system running on: http://localhost:${patternLibraryPort}`,
-    v3: `Component library running on: http://localhost:${componentLibraryPort}`,
-    all: `Design system running on: http://localhost:${patternLibraryPort}\n  you are running both version 3 and 4 of Assets Frontend`
   }
 }
--- gulpfile.js/tasks/server.js
+++ gulpfile.js/tasks/server.js
@@ -1,53 +1,87 @@
 'use strict'
 
 const gulp = require('gulp')
+const portfinder = require('portfinder')
 const gutil = require('gulp-util')
 const browserSync = require('browser-sync')
 const colors = require('colors')
 const config = require('../config').browserSync
-const serverMessage = require('../config').serverMessage
+portfinder.basePort = 9033
 const assetsPathV3 = `http://localhost:9032/public/v3-SNAPSHOT/`
 const assetsPathV4 = `http://localhost:9032/public/v4-SNAPSHOT/`
+// const servers = {}
+// const serverMessage = {
+//   v4: `Design system running on: http://localhost:${portsInUse[0]}`,
+//   v3: `Component library running on: http://localhost:${portsInUse[1]}`,
+//   all: `Design system running on: http://localhost:\n  you are running both version 3 and 4 of Assets Frontend`
+// }
 
 gulp.task('server', () => {
   if (gutil.env.version === 'v3') {
-    browserSync.create().init(config.v4, afVersionMessage(serverMessage.all))
-    browserSync.create().init(config.v3, afVersionMessage(serverMessage.v3))
-    browserSync.create().init(config.assets, assetsMessage(gutil.env.version))
+    startServer('component-library')
+      .then(() => {
+        startServer('design-pattern-library')
+      })
+    browserSync.create().init(config.assets, afStarted)
+    // afVersionMessage(serverMessage.all)
+    // assetsMessage(gutil.env.version)
   } else {
-    browserSync.create().init(config.v4, afVersionMessage(serverMessage.v4))
-    browserSync.create().init(config.assets, assetsMessage(gutil.env.version))
+    startServer('design-pattern-library')
+    browserSync.create().init(config.assets, afStarted)
+    // afVersionMessage(serverMessage.v4)
+    // assetsMessage(gutil.env.version)
   }
 })
 
-const afVersionMessage = (message) => {
-  return console.log(colors.green(`
-  #######################################################
-  
-  ${message}
-  
-  #######################################################
-  `))
+const startServer = (serverPath) => {
+  let serverConfig
+  return new Promise((resolve, reject) => {
+    const hasStarted = () => {
+
+      message(`Started ${serverPath} on port ${serverConfig.port}`)
+      // servers[serverPath] = {}
+      // servers[serverPath]['config'] = serverConfig
+      // console.log(servers)
+      // console.log(serverConfig.port)
+      resolve()
+    }
+    try {
+      getServerConfig(serverPath).then((config) => {
+        serverConfig = config
+        browserSync.create(serverPath).init(config, hasStarted)
+      })
+
+    } catch (err) {
+      reject(err)
+    }
+  })
 }
 
-const assetsMessage = (version) => {
-  if (version === 'v3') {
-    return console.log(colors.yellow(`
-  #######################################################
-  
-  Assets running on:
-  ${assetsPathV4} \n  ${assetsPathV3}
-  
-  #######################################################
+const getServerConfig = (serverPath) => {
+  return new Promise((resolve, reject) => {
+    portfinder.getPort((err, port) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve({
+          ui: false,
+          logLevel: 'silent',
+          port: port,
+          open: false,
+          server: serverPath
+        })
+      }
+    })
+  })
+}
+
+const afStarted = () => {
+  message('assets-frontend started on port 9032')
+
+}
+
+const message = (message) => {
+  return console.log(colors.yellow(`
+  ${message}
   `))
-  } else {
-    return console.log(colors.yellow(`
-  #######################################################
-  
-  Assets running on:
-  ${assetsPathV4}
-  
-  #######################################################
-  `))
-  }
 }
