640a6c9a0b08d2b482c44451d8ffbeed2768f0e6
stevenaproctor <mrproctor@googlemail.com>
2018-04-25 11:32:46 +0100

    Warn users we are going to time them out

--- assets/components/index.js
+++ assets/components/index.js
@@ -2,7 +2,7 @@
 
 var accountMenu = require('./account-menu/account-menu')
 var charCounter = require('./character-counter/character-counter')
-var timeoutDialog = require('../patterns/timeout-dialog/timeoutDialog')
+var timeoutDialog = require('../patterns/warn-users-we-are-going-to-time-them-out/timeoutDialog')
 
 ;(function ($, window, document) {
   accountMenu()
