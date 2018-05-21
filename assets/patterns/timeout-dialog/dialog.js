987ef5aee1b29659d302ab649c94ca812956c95d
Mat Carey <mat+yagni-digital@matcarey.co.uk>
2018-05-21 16:33:13 +0100

    Removed code examples which have now been implemented.

--- assets/patterns/timeout-dialog/dialog.js
+++ assets/patterns/timeout-dialog/dialog.js
@@ -99,52 +99,6 @@ module.exports = {
 //           }
 //         }
 //       }
-//       // AL: add touchmove handler
-//       $(document).on('touchmove', self.handleTouch)
-
-//     addEvents: function () {
-//       var self = this
-//       // trap focus in modal (or browser chrome)
-//       $('a, input, textarea, button, [tabindex]').not('[tabindex="-1"]').on('focus', function (event) {
-//         var modalFocus = document.getElementById('timeout-dialog')
-//         if (modalFocus && self.dialogOpen) {
-//           if (!modalFocus.contains(event.target)) {
-//             event.stopPropagation()
-//             modalFocus.focus()
-//           }
-//         }
-//       })
-//
-//       function handleFocus () {
-//         if (self.dialogOpen) {
-//           // clear the countdown
-//           window.clearInterval(self.countdown)
-//           // calculate remaining time
-//           var expiredSeconds = (Math.round(Date.now() / 1000, 0)) - self.startTime
-//           var currentCounter = settings.countdown - expiredSeconds
-//           self.updateUI(currentCounter)
-//           self.startCountdown(currentCounter)
-//         }
-//       }
-//
-//       // AL: handle browsers pausing timeouts/intervals by recalculating the remaining time on window focus
-//       // need to widen this to cover the setTimeout which triggers the dialog for browsers which pause timers on blur
-//       // hiding this from IE8 and it breaks the reset - to investigate further
-//       if (navigator.userAgent.match(/MSIE 8/) == null) {
-//         // $(window).on("blur", function(){
-//         $(window).off('focus', handleFocus)
-//         $(window).on('focus', handleFocus)
-//         // });
-//       }
-//     },
-
-
-// var escPress = function (event) {
-//   if (event.keyCode === 27) {
-//     keepAliveAndClose()
-//   }
-// }
-
 // <div id="timeout-dialog" class="timeout-dialog" role="dialog" aria-labelledby="timeout-message" tabindex=-1 aria-live="polite">
 // </div>
 // <div id="timeout-overlay" class="timeout-overlay"></div>
