e28858fc65849d48c255c2b613ff6eb669c11eab
Ian Leggett <ileggett@equalexperts.com>
2018-06-06 14:11:44 +0100

    Added CSS rules and a JS file that initialises the GOVUK.ShowHideContent() function from GOV.UK Elements so you can use the conditional reveal on radio buttons.

--- assets/application.js
+++ assets/application.js
@@ -4,6 +4,7 @@ require('jquery')
 require('govuk-template')
 require('javascripts')
 require('./components')
+require('./utils/conditionalReveal')
 var GOVUK = require('stageprompt')
 
 // initialise stageprompt for Analytics
