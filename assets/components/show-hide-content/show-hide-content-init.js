0eb3193b4e1b5c74bfe46fe128a112ae520c9080
Ian Leggett <ileggett@equalexperts.com>
2018-06-06 15:28:56 +0100

    Moved the show hide reveal to the components directory

--- assets/components/show-hide-content/show-hide-content-init.js
+++ assets/components/show-hide-content/show-hide-content-init.js
@@ -1,13 +1,14 @@
+/* global GOVUK */
 var showHide = function () {
   var showHideContent = new GOVUK.ShowHideContent()
   showHideContent.init()
 }
 if (document.addEventListener) {
-  document.addEventListener("DOMContentLoaded", function () {
+  document.addEventListener('DOMContentLoaded', function () {
     showHide()
   })
 } else {
-  window.attachEvent("onload", function () {
+  window.attachEvent('onload', function () {
     showHide()
   })
 }
