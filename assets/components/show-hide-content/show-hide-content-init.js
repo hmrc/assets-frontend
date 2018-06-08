c766742d360feae20ee269280edbd8917e2e3bc3
Ian Leggett <ileggett@equalexperts.com>
2018-06-08 17:20:40 +0100

    Revert "Refactored tests as the previous ones were passing but the code was failing in production."
    This reverts commit 0ac50276e7593f58c645f26a0c21e5e9e827d931.

--- assets/components/show-hide-content/show-hide-content-init.js
+++ assets/components/show-hide-content/show-hide-content-init.js
@@ -3,12 +3,4 @@ var showHide = function () {
   var showHideContent = new GOVUK.ShowHideContent()
   showHideContent.init()
 }
-if (document.addEventListener) {
-  document.addEventListener('DOMContentLoaded', function () {
-    showHide()
-  })
-} else {
-  window.attachEvent('onload', function () {
-    showHide()
-  })
-}
+showHide()
--- assets/components/show-hide-content/show-hide-content-init.test.js
+++ assets/components/show-hide-content/show-hide-content-init.test.js
@@ -3,29 +3,19 @@
 require('jquery')
 
 describe('Show hide content', function () {
-  beforeEach(function (done) {
+  beforeEach(function () {
     jasmine.getFixtures().fixturesPath = 'base/components/show-hide-content/'
     loadFixtures('show-hide-content-init.html')
-    $('html').addClass('js')
-    var event = document.createEvent('Event')
-    event.initEvent('DOMContentLoaded', true, true)
     require('govuk_frontend_toolkit/javascripts/govuk/show-hide-content') // This is added in browserify during a build
     require('../../application')
-    window.document.dispatchEvent(event)
-    done()
+    $('html').addClass('js')
   })
 
-  // Todo: See if we can find a way to check that the showHideContent.init() code has actually been called.
-
-  it('should reveal content when selecting the associated radio button', function (done) {
+  it('should have been called', function () {
+    expect($('#contact-by-phone')).toBeHidden()
     $('#example-contact-by-phone').click()
     expect($('#contact-by-phone')).not.toBeHidden()
-    done()
-  })
-
-  it('should hide content when a different radio button is clicked', function (done) {
     $('#example-contact-by-email').click()
     expect($('#contact-by-phone')).toBeHidden()
-    done()
   })
 })
