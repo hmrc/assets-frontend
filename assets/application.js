e4b503914d0085b5123579799b2da4583b8df574
Gordon McMullan <gordon.mcmullan@digital.hmrc.gov.uk>
2018-12-12 11:56:38 +0000

    PSEC-461 Add an ajaxPrefilter to patch jQuery

--- assets/application.js
+++ assets/application.js
@@ -1,6 +1,14 @@
 window._gaq = window._gaq || []
 
 require('jquery')
+
+// patch jQuery for http://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2015-9251
+window.jQuery.ajaxPrefilter(function (s) {
+  if (s.crossDomain) {
+    s.contents.script = false
+  }
+})
+
 require('govuk-template')
 require('javascripts')
 require('./components')
